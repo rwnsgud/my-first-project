634044cba6b0967cb0b796e17594e4eef594b8e2
rwnsgud <121940373+rwnsgud@users.noreply.github.com>
2024-02-24 16:41:40 +0900

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.Practice.ViewModels;
+using System.Windows;
+
+namespace CommunityToolkit.Mvvm.Practice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainViewModel ViewModel { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ViewModel = new MainViewModel();
+            DataContext = ViewModel;
+        }
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows.Input;
+
+namespace CommunityToolkit.Mvvm.Practice.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        //[ObservableProperty]
+        //private string _textBox;
+
+        //[ObservableProperty]
+        //private string _textBlock;
+
+        //[RelayCommand]
+        //public void ClickOnButton()
+        //{
+        //    TextBlock = TextBox;
+        //}
+
+        private string _textBox;
+        public string TextBox
+        {
+            get => _textBox;
+            set => SetProperty(ref _textBox, value);
+        }
+
+        private string _textBlock;
+        public string TextBlock
+        {
+            get => _textBlock;
+            set => SetProperty(ref _textBlock, value);
+        }
+
+        public MainViewModel()
+        {
+            ClickOnButtonCommand = new RelayCommand(ClickOnButton);
+        }
+        public ICommand ClickOnButtonCommand { get; }
+        private void ClickOnButton() => TextBlock = TextBox;
+    }
+}
