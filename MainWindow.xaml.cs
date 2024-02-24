3709618874eaaee64c017894edfc6132eaa6cd7c
rwnsgud <121940373+rwnsgud@users.noreply.github.com>
2024-02-24 16:44:18 +0900

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,9 +3,9 @@ using System.Windows;
 
 namespace CommunityToolkit.Mvvm.Practice
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    
+
+    
     public partial class MainWindow : Window
     {
         public MainViewModel ViewModel { get; set; }
